21e19b36527450cf953a4f7bcde8f0501345eebd
kimalss22 <kimalss22@gmail.com>
2023-03-22 17:41:21 +0900

    기업회원 지원자 목록 불러오기 기능 Repository Test 구현

--- src/test/java/shop/mtcoding/jobara/apply/ApplyRepositoryTest.java
+++ src/test/java/shop/mtcoding/jobara/apply/ApplyRepositoryTest.java
@@ -11,8 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import shop.mtcoding.jobara.apply.dto.ApplyResp.ApplyJoinBoardAndResume;
-import shop.mtcoding.jobara.apply.dto.ApplyResp.MailDto;
-import shop.mtcoding.jobara.apply.model.Apply;
+import shop.mtcoding.jobara.apply.dto.ApplyResp.ApplyJoinBoardAndUser;
 import shop.mtcoding.jobara.apply.model.ApplyRepository;
 
 @MybatisTest
@@ -42,4 +41,25 @@ public class ApplyRepositoryTest {
         assertThat(EmployeeApplyRespDtoList.get(0).getResume().getTitle()).isEqualTo("뛰어난 컴퓨터 프로그래머 능력을 펼쳐보이겠습니다.");
     }
     
+    @Test
+    public void findByUserIdWithBoardAndBoard_test() throws Exception {
+        // given
+        ObjectMapper om = new ObjectMapper();
+        int userId = 6;
+
+        // when
+        List<ApplyJoinBoardAndUser> EmployeeApplyRespDtoList = applyRepository.findByUserIdWithBoardAndUser(userId);
+        String responseBody = om.writeValueAsString(EmployeeApplyRespDtoList);
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        assertThat(EmployeeApplyRespDtoList.get(0).getId()).isEqualTo(1);
+        assertThat(EmployeeApplyRespDtoList.get(0).getState()).isEqualTo("-1");
+        assertThat(EmployeeApplyRespDtoList.get(0).getBoard().getId()).isEqualTo(1);
+        assertThat(EmployeeApplyRespDtoList.get(0).getBoard().getTitle()).isEqualTo("인공지능 솔루션 (AI Solution) 개발");
+        assertThat(EmployeeApplyRespDtoList.get(0).getUser().getId()).isEqualTo(1);
+        assertThat(EmployeeApplyRespDtoList.get(0).getUser().getRealName()).isEqualTo("박종대");
+        assertThat(EmployeeApplyRespDtoList.get(0).getResume().getId()).isEqualTo(1);
+    }
+
 }
