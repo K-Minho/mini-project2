63e28c483365419d9ff475d812da581e6f505f86
kimalss22 <kimalss22@gmail.com>
2023-03-22 16:24:39 +0900

    구직자 지원 Controller Test 구현

--- src/test/java/shop/mtcoding/jobara/apply/ApplyControllerTest.java
+++ src/test/java/shop/mtcoding/jobara/apply/ApplyControllerTest.java
@@ -1,24 +1,35 @@
 package shop.mtcoding.jobara.apply;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import shop.mtcoding.jobara.user.UserController;
-import shop.mtcoding.jobara.user.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.mtcoding.jobara.apply.dto.ApplyReq.ApplyReqDto;
 import shop.mtcoding.jobara.user.vo.UserVo;
 
-@WebMvcTest(UserController.class)
+@WebMvcTest(ApplyController.class)
 public class ApplyControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper om;
+
     @MockBean
-    private UserService userService;
+    private ApplyService applyService;
 
     private MockHttpSession mockSession;
 
@@ -31,5 +42,24 @@ public class ApplyControllerTest {
         principal.setProfile(null);
         mockSession = new MockHttpSession();
         mockSession.setAttribute("principal", principal);
+    }
+
+    @Test
+    public void apply_test() throws Exception {
+        // given
+        ApplyReqDto applyReqDto = new ApplyReqDto(3, 1);
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/apply")
+                .content(om.writeValueAsString(applyReqDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .session(mockSession));
+        String resp = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + resp);
+
+        // verify
+        resultActions.andExpect(status().is2xxSuccessful());
+        resultActions.andExpect(jsonPath("$.code").value(1));
+        resultActions.andExpect(jsonPath("$.msg").value("지원 성공"));
     }
 }
