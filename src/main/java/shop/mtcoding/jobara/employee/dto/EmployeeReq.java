b28f743197687738fd402e9f3d13dfa6c6d4553f
K-Minho <alsgh9979@naver.com>
2023-03-23 18:21:43 +0900

    EmployeeReq vaild 추가

--- src/main/java/shop/mtcoding/jobara/employee/dto/EmployeeReq.java
+++ src/main/java/shop/mtcoding/jobara/employee/dto/EmployeeReq.java
@@ -2,6 +2,10 @@ package shop.mtcoding.jobara.employee.dto;
 
 import java.util.ArrayList;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,34 +15,76 @@ public class EmployeeReq {
       @Getter
       @Setter
       public static class EmployeeJoinReqDto {
+            @NotEmpty
+            @Min(2)
+            @Max(16)
             private String username;
+            @NotEmpty
+            @Min(2)
+            @Max(16)
             private String password;
+            @NotEmpty
+            @Min(2)
+            @Max(32)
             private String email;
       }
 
       @Getter
       @Setter
       public static class EmployeeLoginReqDto {
+            @NotEmpty
+            @Min(2)
+            @Max(16)
             private String username;
+            @NotEmpty
+            @Min(2)
+            @Max(16)
             private String password;
       }
 
       @Getter
       @Setter
       public static class EmployeeUpdateReqDto {
+            @NotEmpty
+            @Min(2)
+            @Max(16)
             private String password;
+            @NotEmpty
+            @Min(2)
+            @Max(32)
             private String email;
+            @NotEmpty
+            @Min(2)
+            @Max(32)
             private String address;
+            @NotEmpty
+            @Min(2)
+            @Max(64)
             private String detailAddress;
+            @NotEmpty
+            @Min(2)
+            @Max(16)
             private String tel;
+            @NotEmpty
             private EmployeeDto employeeDto;
 
             @Getter
             @Setter
             public static class EmployeeDto {
+
+                  @NotEmpty
+                  @Min(2)
+                  @Max(16)
                   private String realName;
+                  @NotEmpty
+                  @Min(2)
+                  @Max(16)
                   private String education;
+                  @NotEmpty
+                  @Min(2)
+                  @Max(16)
                   private Integer career;
+
             }
       }
 
@@ -52,6 +98,7 @@ public class EmployeeReq {
       @Getter
       @Setter
       public static class EmployeeInsertSkillReqDto {
+            @NotEmpty
             private Integer employeeId;
             private ArrayList<Integer> checkLang;
 
