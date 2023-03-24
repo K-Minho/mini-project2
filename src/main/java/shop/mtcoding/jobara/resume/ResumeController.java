e4311d01a7385b924f71f9c14d7f4c9426372f38
K-Minho <alsgh9979@naver.com>
2023-03-24 15:01:35 +0900

    resume controller 수정

--- src/main/java/shop/mtcoding/jobara/resume/ResumeController.java
+++ src/main/java/shop/mtcoding/jobara/resume/ResumeController.java
@@ -2,9 +2,9 @@ package shop.mtcoding.jobara.resume;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
@@ -17,48 +17,48 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import shop.mtcoding.jobara.common.config.auth.LoginUser;
 import shop.mtcoding.jobara.common.dto.ResponseDto;
 import shop.mtcoding.jobara.resume.dto.ResumeReq.ResumeSaveReq;
 import shop.mtcoding.jobara.resume.dto.ResumeReq.ResumeUpdateReq;
 import shop.mtcoding.jobara.resume.model.Resume;
-import shop.mtcoding.jobara.user.vo.UserVo;
 
 @RestController
+@RequiredArgsConstructor
 public class ResumeController {
 
-    @Autowired
-    private ResumeService resumeService;
-
-    public UserVo setPrincipal() {
-        return new UserVo(1, "ssar", "", "employee");
-    }
+    private final ResumeService resumeService;
+    private final HttpSession session;
 
     @PutMapping("/employee/resume/{id}")
     public ResponseEntity<?> updateResume(@PathVariable Integer id, @Valid @RequestBody ResumeUpdateReq resumeUpdateReq,
             BindingResult bindingResult) {
-        UserVo principal = setPrincipal();
+
+        LoginUser user = (LoginUser) session.getAttribute("loginUser");
         if (resumeUpdateReq.getTitle().isEmpty() || resumeUpdateReq.getTitle().isBlank()) {
             resumeUpdateReq.setTitle("무제");
         }
-        resumeService.updateResume(principal.getId(), resumeUpdateReq);
+        resumeService.updateResume(user.getId(), resumeUpdateReq);
         return new ResponseEntity<>(new ResponseDto<>(1, "작성 완료", null), HttpStatus.OK);
     }
 
     @GetMapping("/employee/resume/{id}")
-    public ResponseEntity<?> saveResumeForm(@PathVariable("id") Integer id, Model model, BindingResult bindingResult) {
-        UserVo principal = setPrincipal();
-        Resume resumePS = resumeService.findById(principal.getId(), id);
+    public ResponseEntity<?> saveResumeForm(@PathVariable("id") Integer id, Model model) {
+
+        LoginUser user = (LoginUser) session.getAttribute("loginUser");
+        Resume resumePS = resumeService.findById(user.getId(), id);
         model.addAttribute("resume", resumePS);
-        return new ResponseEntity<>(new ResponseDto<>(1, "", model), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseDto<>(1, "이력서 상세보기", model), HttpStatus.OK);
     }
 
     @GetMapping("/employee/resume/list")
     public ResponseEntity<?> resumeList(Model model) {
 
-        UserVo principal = setPrincipal();
-        List<Resume> resumeListPS = resumeService.findByUserId(principal.getId());
+        LoginUser user = (LoginUser) session.getAttribute("loginUser");
+        List<Resume> resumeListPS = resumeService.findByUserId(user.getId());
         model.addAttribute("resumeList", resumeListPS);
-        return new ResponseEntity<>(new ResponseDto<>(1, "", model), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseDto<>(1, "이력서 목록", model), HttpStatus.OK);
     }
 
     @GetMapping("/employee/resume/saveForm")
@@ -70,19 +70,19 @@ public class ResumeController {
     @PostMapping("/employee/resume/save")
     public ResponseEntity<?> saveResume(@RequestBody @Valid ResumeSaveReq resumeSaveReq, BindingResult bindingResult) {
 
-        UserVo principal = setPrincipal();
+        LoginUser user = (LoginUser) session.getAttribute("loginUser");
         if (resumeSaveReq.getTitle().isEmpty()) {
             resumeSaveReq.setTitle("무제");
         }
-        resumeService.saveResume(principal.getId(), resumeSaveReq);
-        return new ResponseEntity<>(new ResponseDto<>(1, "작성 완료", resumeSaveReq), HttpStatus.CREATED);
+        resumeService.saveResume(user.getId(), resumeSaveReq);
+        return new ResponseEntity<>(new ResponseDto<>(1, "작성 완료", null), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/employee/resume/{id}")
     public ResponseEntity<?> deleteResume(@PathVariable int id) {
 
-        UserVo principal = setPrincipal();
-        resumeService.deleteResume(id, principal.getId());
+        LoginUser user = (LoginUser) session.getAttribute("loginUser");
+        resumeService.deleteResume(id, user.getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "삭제 완료", null), HttpStatus.OK);
     }
 }
