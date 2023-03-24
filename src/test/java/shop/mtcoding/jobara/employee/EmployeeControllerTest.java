8f2681cf953d8c138c7b499fc48310675d92315c
K-Minho <alsgh9979@naver.com>
2023-03-24 17:54:26 +0900

    list detail test 완료

--- src/test/java/shop/mtcoding/jobara/employee/EmployeeControllerTest.java
+++ src/test/java/shop/mtcoding/jobara/employee/EmployeeControllerTest.java
@@ -1,10 +1,13 @@
 package shop.mtcoding.jobara.employee;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -12,41 +15,42 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.yaml.snakeyaml.events.Event.ID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.RequiredArgsConstructor;
+import shop.mtcoding.jobara.common.config.auth.JwtProvider;
 import shop.mtcoding.jobara.employee.dto.EmployeeJoinBuilder;
 import shop.mtcoding.jobara.employee.dto.EmployeeReq.EmployeeJoinReqDto;
 import shop.mtcoding.jobara.employee.dto.EmployeeReq.EmployeeUpdateReqDto;
 import shop.mtcoding.jobara.employee.dto.EmployeeUpdateBuilder;
-import shop.mtcoding.jobara.user.vo.UserVo;
+import shop.mtcoding.jobara.user.model.User;
 
-@RequiredArgsConstructor
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 public class EmployeeControllerTest {
 
-      private final MockMvc mvc;
+      @Autowired
+      private MockMvc mvc;
 
-      private final ObjectMapper om;
+      @Autowired
+      private ObjectMapper om;
 
       private MockHttpSession mockSession;
 
       @BeforeEach
       public void setUp() {
-            UserVo principal = new UserVo();
-            principal.setId(1);
-            principal.setUsername("ssar");
-            principal.setRole("employee");
-            principal.setProfile(null);
+            User user = new User();
+            user.setId(1);
+            user.setRole("employee");
             mockSession = new MockHttpSession();
-            mockSession.setAttribute("loginUser", principal);
+            mockSession.setAttribute("loginUser", user);
+
       }
 
-      // 현재 보류
       @Test
       public void join_test() throws Exception {
+
             // given
             EmployeeJoinReqDto employeeJoinReqDto = EmployeeJoinBuilder.makejoinReqDto("asdf", "1234", "asdf@asdf.asd");
             String requestBody = om.writeValueAsString(employeeJoinReqDto);
@@ -61,20 +65,63 @@ public class EmployeeControllerTest {
             resultActions.andExpect(status().is2xxSuccessful());
       }
 
+      // 보류
       @Test
       public void update_test() throws Exception {
+
             // given
             int id = 1;
             EmployeeUpdateReqDto employeeUpdateReqDto = EmployeeUpdateBuilder.makeUpdateReqDto("1234", "asdf@asdf.asd",
-                        "부산광역시", "null", "null", "null", "null", 1);
+                        "부산광역시", "부산대로", "01011112222", "김실명", "4년제", 1);
+            User user = (User) mockSession.getAttribute("loginUser");
             String requestBody = om.writeValueAsString(employeeUpdateReqDto);
 
             // when
-            ResultActions resultActions = mvc.perform(post("/employee/update/" +
-                        id).content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE).session(mockSession));
+            System.out.println("req:" + requestBody);
+            ResultActions resultActions = mvc.perform(put("/employee/" + id).content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE).session(mockSession)
+                        .header("Authorization", JwtProvider.create(user)));
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+            System.out.println("resp:" + responseBody);
 
             // then
             resultActions.andExpect(status().is2xxSuccessful());
       }
+
+      @Test
+      public void employeeList_test() throws Exception {
+
+            // given
+            int page = 1;
+            String requestBody = om.writeValueAsString(page);
+
+            // when
+            System.out.println("req:" + requestBody);
+            ResultActions resultActions = mvc.perform(get("/list").content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE));
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+            System.out.println("resp:" + responseBody);
+
+            // then
+            resultActions.andExpect(status().is2xxSuccessful());
+      }
+
+      @Test
+      public void employeeDetail_test() throws Exception {
+
+            // given
+            int id = 1;
+            int resumeId = 1;
+
+            // when
+            ResultActions resultActions = mvc
+                        .perform(get("/user/" + id + "/resume/" + resumeId)
+                                    .contentType(MediaType.APPLICATION_JSON_VALUE));
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+            System.out.println("resp:" + responseBody);
+
+            // then
+            resultActions.andExpect(status().is2xxSuccessful());
+      }
+
 }
