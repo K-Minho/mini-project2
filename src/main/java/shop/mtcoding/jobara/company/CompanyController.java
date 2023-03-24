a557ab5b8d830cbf3714d4e9c2ed4d9aa1c5bb00
kimalss22 <kimalss22@gmail.com>
2023-03-24 15:42:35 +0900

    기업 회원 가입 Controller 메소드 설명 주석 추가

--- src/main/java/shop/mtcoding/jobara/company/CompanyController.java
+++ src/main/java/shop/mtcoding/jobara/company/CompanyController.java
@@ -47,6 +47,7 @@ public class CompanyController {
         // 작성일 : 2023-03-24
         // 수정자 : -
         // 수정일 : -
+
         CompanyInfo companyInfo = companyService.getCompany(id);
         return new ResponseEntity<>(new ResponseDto<>(1, "기업 회원 정보 불러오기 성공", companyInfo), HttpStatus.OK);
     }
@@ -54,6 +55,25 @@ public class CompanyController {
     @PostMapping("/joinCompany")
     public ResponseEntity<?> join(@RequestBody @Valid CompanyJoinReqDto companyJoinReqDto, 
             BindingResult bindingResult) {
+        // 1. 기능 : 기업 회원 가입 기능을 구현하는 메소드
+        // 2. Arguments :
+        // - CompanyJoinReqDto
+        //   (username, password, email, address, detailAddress, companyNumb, companyName)
+        //    username : 최소 문자열길이 2, 최대 문자열길이 16, empty
+        //    password : 최소 문자열길이 2, 최대 문자열길이 32, empty
+        //    email : 최소 문자열길이 2, 최대 문자열길이 32, empty
+        //    address : 최소 문자열길이 2, 최대 문자열길이 32, empty
+        //    detailAddress : 최소 문자열길이 2, 최대 문자열길이 64, empty
+        //    companyNumb : 최소 문자열길이 2, 최대 문자열길이 16, empty
+        //    companyName : 최소 문자열길이 1, 최대 문자열길이 16, empty
+
+        // 3. Return :
+
+        // 작성자 : 김태훈
+        // 작성일 : 2023-03-24
+        // 수정자 : -
+        // 수정일 : -
+        
         companyService.insertCompany(companyJoinReqDto);
         return new ResponseEntity<>(new ResponseDto<>(1, "기업 회원 가입 성공", null), HttpStatus.CREATED);
     }
@@ -62,6 +82,7 @@ public class CompanyController {
     @CompanyCheck
     public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody @Valid CompanyUpdateReqDto companyUpdateReqDto, 
             BindingResult bindingResult) {
+                
         LoginUser loginUser = (LoginUser) session.getAttribute("loginUser");
         companyService.updateCompany(companyUpdateReqDto, loginUser.getId());
         return new ResponseEntity<>(new ResponseDto<>(1, "기업 회원 수정 성공", null), HttpStatus.CREATED);
